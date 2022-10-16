65eff292d91d3d92e3113f1c7d0a5e17a7e9fb77
TheFerrango <llorenzo@outlook.com>
2022-10-16 16:46:19 +0200

    Fixed RRank now nullable

--- Models/Scale.cs
+++ Models/Scale.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace NationStatesAPI.Models
 {
-    [XmlRoot(ElementName="SCALE")]
-	public class Scale {
-		[XmlElement(ElementName="SCORE")]
-		public double Score { get; set; }
-		[XmlElement(ElementName="RANK")]
-		public int Rank { get; set; }
-		[XmlElement(ElementName="RRANK")]
-		public int RRank { get; set; }
-		[XmlAttribute(AttributeName="id")]
-		public int Id { get; set; }
+    [XmlRoot(ElementName = "SCALE")]
+    public class Scale
+    {
+        [XmlElement(ElementName = "SCORE")]
+        public double Score { get; set; }
+        // private string Score { get; set; }
+        [XmlElement(ElementName = "RANK")]
+        public int Rank { get; set; }
+        [XmlElement(ElementName = "RRANK")]
+		public string RRankAsText
+        {
+            get { return (RRank.HasValue) ? RRank.ToString() : null; }
+            set { RRank = !string.IsNullOrEmpty(value) ? int.Parse(value) : default(int?); }
+        }
+        [XmlAttribute(AttributeName = "id")]
+        public int Id { get; set; }
 
-		[XmlElement(ElementName="POINT")]
-		public List<Point> Point { get; set; }
-	}
+        [XmlElement(ElementName = "POINT")]
+        public List<Point> Point { get; set; }
+
+
+        [XmlIgnore]
+        public int? RRank { get; set; }
+    }
 }
